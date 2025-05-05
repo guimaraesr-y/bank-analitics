cec1e8ecaa891e79432b66a35973d30f264a59d9
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-05-04 22:54:02 -0300

    Fix broker module importing Config

--- workers/parser/src/broker/broker.module.ts
+++ workers/parser/src/broker/broker.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RabbitMQBroker } from './infra/adapters/rabbitmq-broker.adapter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [
     {
       provide: 'BrokerInterface',
